1c2d3a3f3596b3fe97f77c11107c224bb72b6701
BrunLobo <pereiralobob@gmail.com>
2025-01-25 20:13:23 -0300

    Desafio de POO em C# realizado em um sistema de Smartphone com sucesso!

--- Program.cs
+++ Program.cs
@@ -1,3 +1,21 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Testando a classe Nokia
+        Nokia nokia = new Nokia(numero: "123456", modelo: "Modelo 1", imei: "111111", memoria: 64);
+
+        nokia.InstalarAplicativo("Facebook");
+        nokia.Ligar();
+
+        Console.WriteLine("\n");
+
+        // Testando a classe Iphone
+        Iphone iphone = new Iphone(numero: "456789", modelo: "Modelo 2", imei: "22222", memoria: 64);
+
+        iphone.InstalarAplicativo("WhatsApp");
+        iphone.ReceberLigacao();
+    }
+}
